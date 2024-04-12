5279926404bc75b62f17bd8185b1ad77cb3da520
LuisMiguelsouzap <luis.m.paulo@aluno.senai.br>
2024-04-12 18:08:43 -0300

    first commit

---
+++ scr/components/Form/ResultImc/style.js
@@ -0,0 +1,27 @@
+//bloco de importação de componentes
+
+import { StyleSheet } from "react-native";
+
+//constante de exibição do site
+const styles = StyleSheet.create({
+    resultImc:{
+        flex: 1,
+        marginTop: 15,
+        paddingTop: 60,
+        borderRadius: 50,
+        alignItems: "center",
+        width:"100%",
+    },
+    information:{
+        fontSize: 18,
+        color: "#FF0043",
+        fontWeight: "bold",
+    },
+    numberImc:{
+        fontSize: 48,
+        color: "#FF0043",
+        fontWeight: "bold",
+    }
+});
+
+export default styles
---
+++ scr/components/Title/style.js
@@ -0,0 +1,18 @@
+//bloco de importação de componentes
+import { StyleSheet } from "react-native";
+
+//constante de exibição 
+const styles = StyleSheet.create({
+    boxTitle:{
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+    },
+    textTitle:{
+        color: "#FF0043",
+        fontSize: 24,
+        fontWeight: "bold",
+    }
+});
+
+export default styles
